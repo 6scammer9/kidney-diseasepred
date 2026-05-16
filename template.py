d895204e39761c74124fe7385be6c142bc5673dc
6scammer9 <zaidshaikh102004@gmail.com>
2026-05-16 16:52:19 +0530

    new changes

--- template.py
+++ template.py
@@ -1,14 +1,11 @@
-import os
-from pathlib import path
-import logging
-
-print("hlo")
-
-
-
-
-
-
-
-
-
+import torch
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+print(torch.__version__ )
+#i=tensor
+ #scaler
+scalar = torch.tensor(7)
+print(scalar)
+print(scalar.ndim)
+print(scalar.item())
